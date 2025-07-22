65fb08169e135ec80d6cdc2a388f93eee5309bf5
Shamoy Baker <insertcod3@gmail.com>
2025-07-22 10:59:46 -0400

    Change Hi to Hello and reduce the time to load to make it be a quick welcome.

--- js/preloader.js
+++ js/preloader.js
@@ -1,7 +1,7 @@
 function fadeloader() {
     $("#preloader").fadeOut("slow");
-    $(".wrapper").removeClass("loading").delay(1100);
+    $(".wrapper").removeClass("loading").delay(100);
 }
 $(window).on("load", function () {
-    window.setTimeout("fadeloader();", 1100)
+    window.setTimeout("fadeloader();", 500)
 });
